61f66d88cad050ea34cbadb7229b6df651ba90f3
Marcus Huderle <huderlem@gmail.com>
2022-04-09 09:36:10 -0500

    Add version and help options

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/huderlem/poryscript-pls/server"
 )
 
+const version = "0.0.1"
+
+func parseOptions() {
+	helpPtr := flag.Bool("h", false, "show poryscript-pls help information")
+	versionPtr := flag.Bool("v", false, "show version of poryscript-pls")
+	flag.Parse()
+
+	if *helpPtr {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if *versionPtr {
+		fmt.Printf("%s\n", version)
+		os.Exit(0)
+	}
+}
+
 func main() {
-	lspServer := server.New()
-	lspServer.Run()
+	parseOptions()
+
+	s := server.New()
+	s.Run()
 }
