dad3b01046791197f162756b463f7966badb419c
Marcus Huderle <huderlem@gmail.com>
2022-06-27 10:07:32 -0500

    Release 1.0.5

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/huderlem/poryscript-pls/server"
 )
 
-const version = "1.0.4"
+const version = "1.0.5"
 
 func parseOptions() {
 	helpPtr := flag.Bool("h", false, "show poryscript-pls help information")
